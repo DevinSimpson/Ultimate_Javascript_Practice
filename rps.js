a168afbc33bd1c996dad0f08218b724b7d8975a9
Devin Simpson <devinsimpsonaz@gmail.com>
2022-04-19 10:19:09 -0700

    add functions for user and computer choice

--- rps.js
+++ rps.js
@@ -6,4 +6,21 @@ let userChoice
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
 }))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length)
+
+    if (randomNumber === 0) {
+        computerChoice = 'rock'
+    }
+    if (randomNumber === 1) {
+        computerChoice = 'scissors'
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'paper'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
