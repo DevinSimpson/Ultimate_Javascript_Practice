349e021f25b31b874b7f754db665e5f789aa2659
Devin Simpson <devinsimpsonaz@gmail.com>
2022-04-26 17:24:56 -0700

    change possible choice to get element by classname rps

--- rps.js
+++ rps.js
@@ -1,7 +1,7 @@
 const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('button')
+const possibleChoices = document.getElementsByClassName('rps')
 let userChoice
 let computerChoice
 let result
