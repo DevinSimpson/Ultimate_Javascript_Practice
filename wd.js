fe3c0926f9ec6ab47968c26bb9281a5ea0743e94
Devin Simpson <devinsimpsonaz@gmail.com>
2022-05-09 15:07:56 -0700

    Create function to create squares for gameboard

---
+++ wd.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    function createSquares() {
+        const gameBoard = document.getElementById("wd-board")
+
+        for (let index = 0; index < Array.length; index++) {
+            const element = array[index];
+        }
+    }
+})
