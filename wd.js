d7dcfeefdd0e0d6dcf47ca3d18eab37eade80692
Devin Simpson <devinsimpsonaz@gmail.com>
2022-05-09 18:28:11 -0700

    Add a eventlistener to wordle keyboard

--- wd.js
+++ wd.js
@@ -4,37 +4,38 @@ document.addEventListener("DOMContentLoaded", () => {
     let guessedWords = [[]]
     let availableSpace = 1;
 
-    const keys = document.querySelectorAll(".keyboard-row button")
+    const keys = document.querySelectorAll(".wd")
 
     for (let i = 0; i < keys.length; i++) {
         keys[i].onclick = ({ target }) => {
             const letter = target.getAttribute("data-key");
 
             updateGuessedWords(letter);
+            console.log(letter);
         };
     }
 
     function getCurrentWordArr() {
-        const numberOfGuessedWords = guessedWords.length
-        return guessedWords[numberOfGuessedWords - 1]
+        const numberOfGuessedWords = guessedWords.length;
+        return guessedWords[numberOfGuessedWords - 1];
     }
 
     function updateGuessedWords(letter) {
-        const currentWordArr = getCurrentWordArr()
+        const currentWordArr = getCurrentWordArr();
 
         if (currentWordArr && currentWordArr.length < 5) {
-            currentWordArr.push(letter)
+            currentWordArr.push(letter);
 
-            const availableSpaceEl = document.getElementById(String(1))
+            const availableSpaceEl = document.getElementById(String(availableSpace));
+
             availableSpace = availableSpace + 1;
-
             availableSpaceEl.textContent = letter;
         }
 
     }
 
     function createSquares() {
-        const gameBoard = document.getElementById("wd-board")
+        const gameBoard = document.getElementById("wd-board");
 
         for (let index = 0; index < 30; index++) {
             let square = document.createElement("div");
